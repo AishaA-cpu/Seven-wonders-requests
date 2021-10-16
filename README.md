472c441d3a99dca377601d793c1c08ff8f6489af
Aisha <ishaarigbabu@gmail.com>
2021-10-16 15:05:20 -0500

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This project tests using the requests lib to use the locationIQ API
+To use requests to gets responses from the py. API
+dependencies used is the requests lib
---
+++ seven_wonders.py
@@ -0,0 +1,6 @@
+# goal of this is to to demonstrate making requests using python code instead of postman(a tool for making API requests and
+# receiving responses)
+# we will be using requests a py. library used to facilitate making api requests, you can also set rate limits so that
+# we dont get an error with requests(read docs)
+# in requests we need to set the path, set the query params like query string, formart etc
+# for apis that need auth keys we also need to set the keys are global constants
